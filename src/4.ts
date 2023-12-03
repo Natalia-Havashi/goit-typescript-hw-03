42c46473c41c4d81c219e99f22086fd7ec4632bf
Natalia-Havashi <natali.gavashi@gmail.com>
2023-12-03 23:40:42 +0200

    ts

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,44 @@
+class Key {
+    private signature: number = Math.random();
+
+    getSignature(): number {
+        return this.signature;
+    }
+}
+
+class Person {
+    constructor(private key: Key) {}
+
+    getKey() {
+        return this.key;
+    }
+}
+
+abstract class House {
+    protected door: boolean = false;
+    protected tenants: Person[] = [];
+
+    constructor(protected key: Key) {}
+
+    abstract openDoor(key: Key): void;
+
+    comeIn(person: Person): void {
+        if (this.door) {
+            this.tenants.push(person);
+            console.log('Вхід дозволено.');
+        }
+    }
+}
+
+class MyHouse extends House {
+    openDoor(key: Key): void {
+        if (key.getSignature() === this.key.getSignature()) {
+            this.door = true;
+            console.log('Двері відчинені.');
+        }
+    }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +47,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
